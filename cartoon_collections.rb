aa6098f10f4287eddcbbc9a6d9bd6275f519139e
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-03-19 07:49:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(array)
+  array.each_with_index do |item, index|
+    indexplus = index + 1
+    puts "#{indexplus} #{item}"
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+# planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+# summon_captain_planet(planeteer_calls)
+#=> ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+
+def summon_captain_planet(array)
+  array.map do |item|
+    item.capitalize!()
+    # item = item.capitalize()
+    item = item.concat(33)
+    print(item)
+  end
+  array
+  # puts array.inspect
+end
+
+
+def long_planeteer_calls(array)
+  array.any? {|item| item.length > 4 }
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect{|cheese| cheese_types.include?(cheese)}
 end
